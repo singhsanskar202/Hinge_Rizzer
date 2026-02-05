ee6317193da208d7bef50204290bf64fe88b973b
singhsanskar202 <72119253+singhsanskar202@users.noreply.github.com>
2026-02-05 14:07:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ GOOGLE_CREDS_JSON = os.environ.get("GOOGLE_CREDS_JSON")
 SPREADSHEET_NAME = "Hinge_Rizz_Tracker"
 WORKSHEET_NAME = "Sheet1"
 
-MODEL_NAME = "mistralai/mistral-small-3.1-24b-instruct:free"
+MODEL_NAME = "google/gemma-3-4b-it:free"
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
