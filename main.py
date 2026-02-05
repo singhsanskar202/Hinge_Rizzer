466bd005cab63062cdb6d8290b595a13a6d89d3e
singhsanskar202 <72119253+singhsanskar202@users.noreply.github.com>
2026-02-05 12:09:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ GOOGLE_CREDS_JSON = os.environ.get("GOOGLE_CREDS_JSON")
 SPREADSHEET_NAME = "Hinge_Rizz_Tracker"
 WORKSHEET_NAME = "Sheet1"
 
-MODEL_NAME = "allenai/molmo-2-8b:free"
+MODEL_NAME = "nvidia/nemotron-nano-12b-v2-vl:free"
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
